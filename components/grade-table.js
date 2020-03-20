37167dd1514f1311480a5288e7e0357987132b11
Eric Lowry <ericmichaellowry@gmail.com>
2020-03-20 13:02:00 -0700

    Set up new GradeTable class and added constructor and getGrades methods

---
+++ components/grade-table.js
@@ -0,0 +1,11 @@
+class GradeTable{
+  constructor(tableElement){
+    this.tableElement = tableElement;
+    this.updateGrades = this.updateGrades.bind(this);
+  }
+
+  updateGrades(grades){
+    console.log(grades);
+  }
+
+}
