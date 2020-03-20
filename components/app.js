827ac435cc68b597b24cd7ab7bd64644bff0b582
Eric Lowry <ericmichaellowry@gmail.com>
2020-03-20 12:40:30 -0700

    Created App class with methods to start, call for grades, and handle status. Instantiated new app and called start.

---
+++ components/app.js
@@ -0,0 +1,30 @@
+class App{
+  constructor(){
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
+
+  handleGetGradesError(error){
+    console.error(error);
+  }
+
+  handleGetGradesSuccess(grades){
+    console.log(grades);
+  }
+
+  getGrades(){
+    $.ajax({
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      method: "GET",
+      headers: {
+        "X-Access-Token": "U8ZjMVRH"
+      },
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError
+    })
+  }
+
+  start(){
+    this.getGrades();
+  }
+}
