dc5f4871c22e65b7d137701b324f28d5738331c4
Eric Lowry <ericmichaellowry@gmail.com>
2020-03-22 13:41:24 -0700

    added GradeForm.handleSubmit functionality to prevent default and capture form data

--- components/grade-form.js
+++ components/grade-form.js
@@ -1,8 +1,10 @@
 class GradeForm{
   constructor(formElement){
-    this.formElement = formElement.addEventListener("submit", this.handleSubmit);
+    this.formElement = formElement;
     this.createGrade = null;
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.formElement.addEventListener("submit", this.handleSubmit);
   }
 
   onSubmit(createGrade){
@@ -10,8 +12,15 @@ class GradeForm{
   }
 
   handleSubmit(event){
-    event.preventDefautl();
-    console.log("GradeForm.handleSubmit fired")
+    event.preventDefault();
+    var target = event.target;
+    var formData = new FormData(target);
+    var nameInput = formData.get("name");
+    var courseInput = formData.get("course");
+    var gradeInput = formData.get("grade");
+
+    this.createGrade(nameInput, courseInput, gradeInput);
+    target.reset();
   }
 
 }
